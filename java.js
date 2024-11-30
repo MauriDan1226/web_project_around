da6ad813c1aee2bc075a166b2a77b104da3f621d
titanram <mdrg.mauricio@outlook.com>
2024-11-29 19:22:31 -0600

    avances

--- java.js
+++ java.js
@@ -1,1 +1,11 @@
-console.log(8**2);
+const popup=document.querySelector(".dialog")
+popup.showModal();
+
+const button=document.querySelector(".dialog__button")
+button.addEventListener("click", function unclick(){
+    console.log ("hola");
+    console.log (button.textContent)
+    console.log (popup.innerHTML)
+    popup.classList.add("dialog2")
+/*     popup.classList.remove("dialog")
+ */})
